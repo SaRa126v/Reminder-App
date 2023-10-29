f345517793fc8402c715a66d3c1e7c304bdad71e
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-10-29 21:42:35 +0330

    update css , start js
    add blur and pop up

---
+++ app.js
@@ -0,0 +1,66 @@
+// variables...........................................
+const addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", displayGp);
+
+const groupsSec = document.querySelector("#groupsSec");
+const blurPage = document.querySelector("#blurPage");
+
+// bluring the background & displaying the pop up
+function displayGp() {
+  // blur the background
+  document.querySelector(".disabled")?.classList.remove("disabled");
+
+  // add only one section of groups
+  if (!document.querySelector("#groups")) {
+    groupsSec.insertAdjacentHTML("afterbegin", gpTemp());
+  }
+
+  // go to the next page............
+  const groups = document.querySelectorAll("#groups > div");
+
+  // for each gp of task
+  groups.forEach((group) => {
+    group.addEventListener("click", nextPage);
+  });
+}
+
+// a template for dsplaying groups of tasks
+function gpTemp() {
+  return `<div id="groups">
+    
+    <!-- gp1 -->
+      <div id="customGp"><a href="Two Page/index.Html"><img src="assets/images/custom gp.png"><h2>Custom</h2></a></div>
+    
+      <!-- gp2 -->
+      <div id="specialGp"><a href="Two Page/index.Html"><img src="assets/images/special gp.png"><h2>Special
+    Event</h2></a></div>
+    
+    <!-- gp3 -->
+      <div id="routineGp"><a href="Two Page/index.Html"><img src="assets/images/routine gp.png"><h2>Routine</h2></a></div>
+
+      </div>`;
+}
+
+// get the id of clicked element in order to go to the specific page chosen by the user
+function nextPage(e) {
+//   e.preventDefault();
+
+  switch (e.target.parentElement.parentElement.id) {
+    case "customGp":
+      console.log("1");
+      break;
+
+    case "specialGp":
+      console.log("2");
+      break;
+
+    case "routineGp":
+      console.log("3");
+      break;
+  }
+}
+
+
+function optionMaker() {
+
+}
