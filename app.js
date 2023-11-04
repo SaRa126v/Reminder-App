bad96d8b6dc436298b0967d0ac240cd52287269c
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-11-04 11:46:03 +0330

    update local storage of page 1
    fix nav bar section : by clicking on the container the blur & task gps disappear

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 // import {  } from "./Two Page/app.js";
 
-
 // variables...........................................
+
+// add task button:
 const addBtn = document.querySelector("#addBtn");
+// group of tasks section:
+const groupsSec = document.querySelector(".groupsSec");
+// the blur container:
+const blurPage = document.querySelector("#blurPage");
+// each gp of tasks:
+const groups = document.querySelectorAll("#groups > div");
+
+// .....................................................
+
+// Events.............................................
+
+// add task btn is clicked
 addBtn.addEventListener("click", displayGp);
 
-const groupsSec = document.querySelector("#groupsSec");
-const blurPage = document.querySelector("#blurPage");
+// window is clicked
+window.addEventListener("click", hideGp);
+
+// each gp of task is clicked
+groups.forEach((group) => {
+  group.addEventListener("click", nextPage);
+});
+
+// .....................................................
+
 
 // bluring the background & displaying the pop up
 function displayGp() {
   // blur the background
-  document.querySelector(".disabled")?.classList.remove("disabled");
+  document.querySelectorAll(".disabled").forEach((disabledEl) => {
+    disabledEl.classList.remove("disabled");
+  });
 
   // add only one section of groups
   if (!document.querySelector("#groups")) {
     groupsSec.insertAdjacentHTML("afterbegin", gpTemp());
   }
-
-  // go to the next page............
-  const groups = document.querySelectorAll("#groups > div");
-
-  // for each gp of task
-  groups.forEach((group) => {
-    // group.addEventListener("click", nextPage);
-  });
 }
 
 // a template for dsplaying groups of tasks
@@ -44,24 +59,65 @@ function gpTemp() {
       </div>`;
 }
 
+// if user clicks on the blur container the task groups should disappear
+function hideGp(e) {
+  if (Object.is(e.target, blurPage)) {
+    blurPage?.classList.add("disabled");
+    groupsSec?.classList.add("disabled");
+  }
+}
+// ....................................................
+
 // get the id of clicked element in order to go to the specific page chosen by the user
-// give options to the optionMaker func to build category for its task gp 
-// function nextPage(e) {
+// give options to the optionMaker func to build category for its task gp
+function nextPage(e) {
+  // the default array of task options
+  let taskArray = [
+    "General",
+    "Work",
+    "Shopping",
+    "Personal",
+    "Education",
+    "Vacation",
+  ];
 
-//   switch (e.target.parentElement.parentElement.id) {
-//     // custom task gp
-//     case "customGp":
-//       optionMaker(["General","Work","Shopping","Personal","Education","Vacation"]);
-//       break;
+  // the chosen array of task options
+  switch (e.target.parentElement.parentElement.id) {
+    // custom task gp
+    case "customGp":
+      taskArray = [
+        "General",
+        "Work",
+        "Shopping",
+        "Personal",
+        "Education",
+        "Vacation",
+      ];
+      break;
 
-//       // special event task gp
-//     case "specialGp":
-//       optionMaker(["Party","Celebration","Conference","Wedding","Birthday","Concert"]);
-//       break;
+    // special event task gp
+    case "specialGp":
+      taskArray = [
+        "Party",
+        "Celebration",
+        "Conference",
+        "Wedding",
+        "Birthday",
+        "Concert",
+      ];
+      break;
 
-//       // routine task gp
-//     case "routineGp":
-//       optionMaker(["Pill","makeup","Reading","Pray","Exercise","Health"]);
-//       break;
-//   }
-// }
+    // routine task gp
+    case "routineGp":
+      taskArray = ["Pill", "makeup", "Reading", "Pray", "Exercise", "Health"];
+      break;
+  }
+
+  console.log(taskArray);
+  return taskArray;
+}
+
+// ....................................................
+
+// save the array of chosen task gp in the local storage
+// nextPage();
